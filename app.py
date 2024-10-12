6b28150547668a59a35ac9aa57424e04537df591
Vyshnavtr0 <fs7468726@gmail.com>
2024-10-12 20:42:23 +0530

    done

--- app.py
+++ app.py
@@ -1,34 +1,11 @@
-from flask import Flask, request, send_file
-from rembg import remove
-from PIL import Image
-import io
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/remove-bg', methods=['POST'])
-def remove_background():
-    # Check if the request contains a file
-    if 'file' not in request.files:
-        return {"error": "No file provided"}, 400
-    
-    file = request.files['file']
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    if file.filename == '':
-        return {"error": "No file selected"}, 400
-
-    # Read the image file
-    input_image = Image.open(file.stream)
-    
-    # Remove the background
-    output_image = remove(input_image)
-
-    # Create a BytesIO object to save the image in memory
-    img_byte_arr = io.BytesIO()
-    output_image.save(img_byte_arr, format='PNG')
-    img_byte_arr.seek(0)
-
-    # Send the processed image back to the client
-    return send_file(img_byte_arr, mimetype='image/png', as_attachment=True, download_name='output.png')
 
 if __name__ == '__main__':
     app.run(debug=True)
